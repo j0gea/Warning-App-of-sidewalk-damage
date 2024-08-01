fc9462eda902cb493989ce5272d049f2db20ec30
KSJ-0 <kim.mail1124@gmail.com>
2024-08-02 06:41:01 +0900

    OpenCV

---
+++ CameraEx/OpenCV/build/intermediates/cxx/Debug/5o50523h/logs/x86_64/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\CookAndroid\\SDK\\cmake\\3.18.1\\bin\\cmake.exe" ^
+  "-HC:\\CookAndroid\\Project\\capston\\Warning-App-of-sidewalk-damage\\CameraEx\\OpenCV\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=26" ^
+  "-DANDROID_PLATFORM=android-26" ^
+  "-DANDROID_ABI=x86_64" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86_64" ^
+  "-DANDROID_NDK=C:\\CookAndroid\\SDK\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\CookAndroid\\SDK\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\CookAndroid\\SDK\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\CookAndroid\\SDK\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\CookAndroid\\Project\\capston\\Warning-App-of-sidewalk-damage\\CameraEx\\OpenCV\\build\\intermediates\\cxx\\Debug\\5o50523h\\obj\\x86_64" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\CookAndroid\\Project\\capston\\Warning-App-of-sidewalk-damage\\CameraEx\\OpenCV\\build\\intermediates\\cxx\\Debug\\5o50523h\\obj\\x86_64" ^
+  "-DCMAKE_BUILD_TYPE=Debug" ^
+  "-BC:\\CookAndroid\\Project\\capston\\Warning-App-of-sidewalk-damage\\CameraEx\\OpenCV\\.cxx\\Debug\\5o50523h\\x86_64" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
---
+++ CameraEx/OpenCV/build/intermediates/cxx/Debug/o6t2s4c4/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\CookAndroid\\SDK\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\CookAndroid\\Project\\capston\\Warning-App-of-sidewalk-damage\\CameraEx\\OpenCV\\.cxx\\Debug\\o6t2s4c4\\x86_64" ^
+  opencv_jni_shared
