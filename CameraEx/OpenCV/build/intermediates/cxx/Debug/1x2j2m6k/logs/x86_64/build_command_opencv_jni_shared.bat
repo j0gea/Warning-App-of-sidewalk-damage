061dd93aaaf81a227d75c772238d12ad04e8654b
Merona00 <clairdelune_00@naver.com>
2024-08-18 01:03:53 +0900

    카메라뷰랑 UI연동

---
+++ CameraEx/OpenCV/build/intermediates/cxx/Debug/1x2j2m6k/logs/x86_64/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\user\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\user\\AndroidStudioProjects\\Warning-App-of-sidewalk-damage\\CameraEx\\OpenCV\\.cxx\\Debug\\1x2j2m6k\\x86_64" ^
+  opencv_jni_shared
