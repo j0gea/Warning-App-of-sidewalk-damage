a713311d6d87769397fa0f5f1cd8a909b9ab2957
KSJ-0 <kim.mail1124@gmail.com>
2024-08-24 02:56:47 +0900

    OpenCV 삭제

--- CameraEx/OpenCV/build/intermediates/cxx/Debug/o6t2s4c4/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\CookAndroid\\SDK\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\CookAndroid\\Project\\capston\\Warning-App-of-sidewalk-damage\\CameraEx\\OpenCV\\.cxx\\Debug\\o6t2s4c4\\x86_64" ^
-  opencv_jni_shared
